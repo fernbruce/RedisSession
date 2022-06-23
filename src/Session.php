52c24f871458e55600120317284b2f1cd4781b4c
wpro <171006308@qq.com>
2022-06-23 10:00:00 +0800

    modify

---
+++ src/Session.php
@@ -0,0 +1,123 @@
+<?php
+
+
+namespace Administrator\RedisSession;
+
+
+/**
+ * Class session
+ */
+class session
+{
+    /**
+     * @var Redis
+     */
+    private $storage;
+
+    /**
+     * @var int
+     */
+    private $ttl;
+
+    /**
+     * @var string
+     */
+    private $prefix;
+
+    /**
+     * session constructor.
+     * @param $storage
+     * @param string $prefix
+     * @param int $ttl
+     */
+    public function __construct($storage, $prefix = 'PHPSESSID:', $ttl = 3600)
+    {
+        $this->storage = $storage;
+        $this->ttl = $ttl;
+        $this->prefix = $prefix;
+        session_set_save_handler(
+            array(&$this, 'open'),
+            array(&$this, 'close'),
+            array(&$this, 'read'),
+            array(&$this, 'write'),
+            array(&$this, 'destroy'),
+            array(&$this, 'gc')
+        );
+        session_start();
+    }
+
+    /**
+     * @param $savePath
+     * @param $sessionName
+     * @return bool
+     */
+    function open($savePath, $sessionName)
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    function close()
+    {
+        return true;
+    }
+
+    /**
+     * @param $id
+     * @return string
+     */
+    function read($id)
+    {
+        $id = $this->prefix . $id;
+        $sessData = $this->storage->get($id);
+        $this->storage->expire($id, $this->ttl);
+        return $sessData ?: '';
+    }
+
+    /**
+     * @param $id
+     * @param $sess_data
+     * @return bool
+     */
+    function write($id, $sessionData)
+    {
+        $id = $this->prefix . $id;
+        $this->storage->set($id, $sessionData);
+        $this->storage->expire($id, $this->ttl);
+        return true;
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    function destroy($id)
+    {
+        $this->storage->del($this->prefix . $id);
+        return true;
+    }
+
+    /**
+     * @param $maxLifeTime
+     * @return bool
+     */
+    function gc($maxLifeTime)
+    {
+        return true;
+    }
+}
+
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', '6379');
+$session = new session($redis);
+//@session_start();
+
+$_SESSION['age'] = 11;
+$_SESSION['name'] = 'jack';
+$_SESSION['edu'] = '财经学院';
+$_SESSION['hobby'] = '唱歌';
+$_SESSION['parttime'] = '摄影';
+var_dump($_SESSION);;
